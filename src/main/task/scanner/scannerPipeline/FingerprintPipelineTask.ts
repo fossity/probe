f7766feb4982fd075a518457ac90bb4acff2f071
Agustin Groh <agusgroh@gmail.com>
2023-01-23 11:40:24 -0300

    CE-1375 Adds dependency scanner on pipeline

--- src/main/task/scanner/scannerPipeline/FingerprintPipelineTask.ts
+++ src/main/task/scanner/scannerPipeline/FingerprintPipelineTask.ts
@@ -2,12 +2,14 @@ import { Project } from '../../../workspace/Project';
 import { ScannerPipeline } from './ScannerPipeline';
 import { CodeIndexTreeTask } from '../../IndexTreeTask/CodeIndexTreeTask';
 import { FingerprintTask } from '../scan/FingerprintTask';
+import { DependencyTask } from '../dependency/DependencyTask';
 
 export class FingerprintPipelineTask extends ScannerPipeline{
   public async run(project: Project): Promise<boolean> {
     const { metadata } = project;
       this.queue.push(new CodeIndexTreeTask(project));
       this.queue.push(new FingerprintTask(project));
+      this.queue.push(new DependencyTask(project));
 
     for await (const [index, task] of this.queue.entries()) {
       await this.executeTask(task, index);
