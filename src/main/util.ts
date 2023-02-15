d53a7e21fbfac15c64af01f1d0a7e3adf9f1fbf7
Agustin Groh <agusgroh@gmail.com>
2023-02-15 10:00:55 -0300

    CE-1454 Avoids removed files on attach task

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,6 +1,7 @@
 /* eslint import/prefer-default-export: off, import/no-mutable-exports: off */
 import { URL } from 'url';
 import path from 'path';
+import fs, {constants} from "fs";
 
 export let resolveHtmlPath: (htmlFileName: string) => string;
 
@@ -17,8 +18,14 @@ if (process.env.NODE_ENV === 'development') {
   };
 }
 
-
-
+export function fileExist(path:string) {
+  return new Promise<boolean>((resolve, reject)=>{
+    fs.access(path,constants.F_OK,(err)=>{
+      if(err) resolve(false);
+      resolve(true);
+    })
+  });
+}
 
 export const getAssetFolderPath = () => {
   const isDev = process.env.NODE_ENV !== 'production';
